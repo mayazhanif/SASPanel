640ce7f4494153b77b2a6fdc7ae9d12fed738d39
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-01-15 20:01:33 +0500

    Update DB, Update Login With Email
    Update DB

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -1,46 +1,35 @@
+from flask import flash
+
 from Database.DbConfig import mysqlconnection
 from app import *
 
 
 @routes.route('/login/', methods=['GET', 'POST'])
-def login():
-    #print(session['id']);
-    #username = request.form['username']
-    #print(mysqlconnection)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'logintype' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
+def login(msg=""):
+    if request.method == 'POST' and 'Email' in request.form and 'password' in request.form and 'logintype' in request.form:
+        Email = request.form['Email']
         password = request.form['password']
         logintype = request.form['logintype']
-        session['loggedin'] = True
-        session['id'] = 1
-        session['username'] = username
-        #session['usertype'] = request.form['logintype']
         if logintype == "Admin":
-            session['usertype'] = "Admin"
+            #session['usertype'] = "Admin"
             return redirect(url_for('routes.admin_dashboard'))
         elif logintype == "User":
-            session['usertype'] = "User"
-            return redirect(url_for('routes.user_dashboard'))
+            cursor = mysqlconnection.cursor()
+            cursor.execute('SELECT * FROM users WHERE User_email = %s AND User_Password = %s', (Email, password))
+            result = cursor.fetchone()
+            #print(result)
+            if result==None:
+                #return redirect(url_for('routes.login'))
+                #flash('You were successfully logged in')
+                return render_template('authentication/login.html', msg="Error")
+            else:
+                session['usertype'] = "Admin"
+                session['loggedin'] = True
+                session['id'] = 1
+                session['username'] = Email
+                return redirect(url_for('routes.user_dashboard'))
         else:
             return render_template('authentication/login.html', msg="Error")
-        # Check if account exists using MySQL
-        cursor = mysqlconnection.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-                # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return redirect(url_for('home'))
-        else:
-            return ""
-            # Account doesnt exist or username/password incorrect
-            #flash("Incorrect username/password!", "danger")
     return render_template('authentication/login.html',title="Login")
 
 
