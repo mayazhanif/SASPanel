86074220b6ceeaa9cbacc26dc262d5b0b0fd6062
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-03-28 12:37:11 +0500

    Local Config

--- Database/DbConfig.py
+++ Database/DbConfig.py
@@ -1,14 +1,18 @@
 import mysql.connector
 import configparser
 import os
-
+localconfig=True
 currentDirectory = os.path.dirname(os.path.abspath(__file__))
 initfile = os.path.join(currentDirectory, 'DB.txt')
 
 
 config = configparser.RawConfigParser()
 config.read(initfile)
-DatabaseDetails = dict(config.items('configlocal'))
+if localconfig==True:
+  DatabaseDetails = dict(config.items('configlocal'))
+else:
+  DatabaseDetails = dict(config.items('config'))
+
 mysqlconnection = None
 try:
   #print(DatabaseDetails['password'])
