f2360f584dd854ade6160f8d1c314a81bb315cd1
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-03 12:05:06 +0500

    UPdated Files

--- functions.py
+++ functions.py
@@ -156,8 +156,44 @@ def add_ftp(username, password):
     #os.system("/bin/bash add_vhost.sh " + username+" "+domain)
 
     
-def set_mysql_root(password):
+def set_mysql_root_old(password):
     os.system("/bin/bash mysql_root.sh " + password)
+
+
+def set_mysql_root(password):
+    root_mysql = '''#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+pwd=$1
+service mysqld stop
+mysqld_safe --skip-grant-tables&
+echo '正在修改密码...';
+echo 'The set password...';
+sleep 6
+m_version=$(cat /www/server/mysql/version.pl|grep -E "(5.1.|5.5.|5.6.)")
+if [ "$m_version" != "" ];then
+    mysql -uroot -e "insert into mysql.user(Select_priv,Insert_priv,Update_priv,Delete_priv,Create_priv,Drop_priv,Reload_priv,Shutdown_priv,Process_priv,File_priv,Grant_priv,References_priv,Index_priv,Alter_priv,Show_db_priv,Super_priv,Create_tmp_table_priv,Lock_tables_priv,Execute_priv,Repl_slave_priv,Repl_client_priv,Create_view_priv,Show_view_priv,Create_routine_priv,Alter_routine_priv,Create_user_priv,Event_priv,Trigger_priv,Create_tablespace_priv,User,Password,host)values('Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','root',password('${pwd}'),'127.0.0.1')"
+    mysql -uroot -e "insert into mysql.user(Select_priv,Insert_priv,Update_priv,Delete_priv,Create_priv,Drop_priv,Reload_priv,Shutdown_priv,Process_priv,File_priv,Grant_priv,References_priv,Index_priv,Alter_priv,Show_db_priv,Super_priv,Create_tmp_table_priv,Lock_tables_priv,Execute_priv,Repl_slave_priv,Repl_client_priv,Create_view_priv,Show_view_priv,Create_routine_priv,Alter_routine_priv,Create_user_priv,Event_priv,Trigger_priv,Create_tablespace_priv,User,Password,host)values('Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','root',password('${pwd}'),'localhost')"
+    mysql -uroot -e "UPDATE mysql.user SET password=PASSWORD('${pwd}') WHERE user='root'";
+else
+    mysql -uroot -e "UPDATE mysql.user SET authentication_string='' WHERE user='root'";
+    mysql -uroot -e "FLUSH PRIVILEGES";
+    mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${pwd}';";
+fi
+mysql -uroot -e "FLUSH PRIVILEGES";
+pkill -9 mysqld_safe
+pkill -9 mysqld
+sleep 2
+service mysqld start
+
+echo '==========================================='
+echo "root密码成功修改为: ${pwd}"
+echo "The root password set ${pwd}  successuful"''';
+
+    WriteFile('mysql_root.sh', root_mysql)
+    os.system("/bin/bash mysql_root.sh " + password)
+    #os.system("rm -f mysql_root.sh")
+
 def install():
     install_packages()
     print("Hello")
