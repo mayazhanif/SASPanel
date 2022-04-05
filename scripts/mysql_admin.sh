1ddc1a484902e0d7d8fabb08cd259d71e4bd30aa
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-05 20:19:29 +0500

    MYSQL FIXES

--- scripts/mysql_admin.sh
+++ scripts/mysql_admin.sh
@@ -2,11 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 pwd=$1
-mysql -u root -e "CREATE USER 'root'@'localhost' IDENTIFIED BY 'Master786';FLUSH PRIVILEGES;"
-mysql -u root -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Master786';FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE USER 'root'@'localhost' IDENTIFIED BY '${pwd}';FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY '${pwd}';FLUSH PRIVILEGES;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';FLUSH PRIVILEGES;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';FLUSH PRIVILEGES;"
-mysql -u root -e "alter user 'root'@'localhost' identified by 'Master786';FLUSH PRIVILEGES;"
+mysql -u root -e "alter user 'root'@'localhost' identified by '${pwd}';FLUSH PRIVILEGES;"
 #mysql -u root -e 'use mysql;update user set plugin="mysql_native_password";FLUSH PRIVILEGES;';
 #mysql -hlocalhost -e "FLUSH PRIVILEGES;alter user 'root'@'localhost' identified by '${pwd}';FLUSH PRIVILEGES;";
 #echo "Removing skip-grant-tables"
