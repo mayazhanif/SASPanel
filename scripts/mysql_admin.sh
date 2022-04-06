7e84d59784e66aad38d9bd341a2f967d19152459
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-06 09:28:51 +0500

    Updated

--- scripts/mysql_admin.sh
+++ scripts/mysql_admin.sh
@@ -6,6 +6,8 @@ mysql -u root -e "CREATE USER 'root'@'localhost' IDENTIFIED BY '${pwd}';FLUSH PR
 mysql -u root -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY '${pwd}';FLUSH PRIVILEGES;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost';FLUSH PRIVILEGES;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';FLUSH PRIVILEGES;"
+mysql -u root -e "create database saspanel;FLUSH PRIVILEGES;"
+mysql -u root saspanel < database.sql
 #mysql -u root -e "alter user 'root'@'localhost' identified by '${pwd}';FLUSH PRIVILEGES;"
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${pwd}';;FLUSH PRIVILEGES;"
 #mysql -u root -e 'use mysql;update user set plugin="mysql_native_password";FLUSH PRIVILEGES;';
