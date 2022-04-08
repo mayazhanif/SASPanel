2680e6ce7ced639095b85420c037c5d1fd009c28
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-08 21:06:16 +0500

    Flask

--- routes/login_routes.py
+++ routes/login_routes.py
@@ -1,12 +1,22 @@
 from flask import flash, request, redirect, url_for
-
 from Database.DbConfig import mysqlconnection
 from app import *
 from functions import *
 import hashlib
-
 from routes import routes
+from flask import current_app as app
 
+@routes.route('/readFile')
+def readFile():
+    f = open("C:\\Users\\mayaz\\Desktop\\testfile.txt", "r")
+    data=f.read()
+    Result = '{ "data":"'+data+'"}'
+    response = app.response_class(
+        response=Result,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @routes.route('/login/', methods=['GET', 'POST'])
 def login(msg=""):
