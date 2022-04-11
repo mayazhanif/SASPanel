e46b5fb5a8281034612801d38ff27cd59f8c3285
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-11 13:07:07 +0500

    Remove Domain and Subdomain

--- functions.py
+++ functions.py
@@ -169,7 +169,8 @@ def add_vhost(username, domain):
 
 def remove_vhost(domain):
     os.system("rm /etc/nginx/sites-enabled/"+domain+"-vhost.conf")
-    os.system("mv /etc/nginx/sites-enabled/"+domain+"-vhost.conf /etc/nginx/backupDomains/")
+    os.system("systemctl restart nginx")
+    #os.system("mv /etc/nginx/sites-available/"+domain+"-vhost.conf /etc/nginx/backupDomains/")
 
 def add_ftp_only(username, password):
     os.system("useradd -p `openssl passwd -1 "+password+"` "+username+"")
