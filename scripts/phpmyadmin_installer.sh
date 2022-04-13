e76e28411fd7e6d15c112246a81a2f7a91dae785
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-13 10:01:57 +0500

    Remove Domain and Subdomain

--- scripts/phpmyadmin_installer.sh
+++ scripts/phpmyadmin_installer.sh
@@ -81,5 +81,15 @@ location /webftp {
     }
 }
 EOF
+cp /usr/share/roundcube/config/config.inc.php.sample /usr/share/roundcube/config/config.inc.php
+sed -i "s|^\(\$config\['db_dsnw'\] =\).*$|\1 \'mysqli://roundcube:${pwd}@localhost/roundcubedb\';|" /usr/share/roundcube/config/config.inc.php
+sed -i "s|^\(\$config\['smtp_server'\] =\).*$|\1 \'localhost\';|" /usr/share/roundcube/config/config.inc.php
+sed -i "s|^\(\$config\['smtp_user'\] =\).*$|\1 \'\';|" /usr/share/roundcube/config/config.inc.php
+sed -i "s|^\(\$config\['smtp_pass'\] =\).*$|\1 \'\';|" /usr/share/roundcube/config/config.inc.php
+sed -i "s|^\(\$config\['smtp_port'\] =\).*$|\1 \25;|" /usr/share/roundcube/config/config.inc.php
+#sed -i "s|^\(\$config\['support_url'\] =\).*$|\1 \'mailto:${E}\';|" /var/www/html/roundcube/config/config.inc.php
+deskey=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9-_#&!*%?' | fold -w 24 | head -n 1)
+sed -i "s|^\(\$config\['des_key'\] =\).*$|\1 \'${deskey}\';|" /usr/share/roundcube/config/config.inc.php
+rm -rf /usr/share/roundcube/installer
 #echo -e "\n\$cfg['Servers'][\$i]['auth_type'] = 'signon';\n\$cfg['Servers'][\$i]['SignonSession'] = 'SignonSession';\n\$cfg['Servers'][\$i]['SignonURL'] = 'sso.php';\n" >> /usr/share/phpmyadmin/config.inc.php
 service nginx restart
