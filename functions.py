b751ab22e9343e1ae7100ec7d44a7ecac6554619
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-30 19:19:17 +0500

    Updated Files

--- functions.py
+++ functions.py
@@ -574,6 +574,7 @@ def install_packages(root_password, mail_password,domain,emailaddress,emailpassw
     os.system("cp /home/SASPanel/scripts/saspanel.service /etc/systemd/system")
     os.system("systemctl daemon-reload")
     os.system("systemctl enable saspanel")
+    os.system("systemctl restart saspanel")
     #os.system("/bin/bash scripts/packages_installer.sh "+root_password+" "+mail_password)
     print("Install packages Completed.")
     #os.system("/bin/bash nginx_config.sh")
