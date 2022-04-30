5065a66ba594791070eff2ecb02a8e6de48f59fc
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-04-30 12:29:48 +0500

    Updated Files

--- Database/DbConfig.py
+++ Database/DbConfig.py
@@ -57,8 +57,6 @@ def mysql_connect():
       user=DatabaseDetails['user'],
       password=DatabaseDetails['password'],
       database=DatabaseDetails['database'],
-      connection_timeout = 160,
-      interactive_timeout = 86400
     )
     #mysqlconnection= "testValue"
   except:
