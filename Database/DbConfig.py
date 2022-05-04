fce030ca2112ba9ae515507406ed28f46d2f1b97
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-05-04 18:29:26 +0500

    Updated Files

--- Database/DbConfig.py
+++ Database/DbConfig.py
@@ -41,7 +41,7 @@ def WriteMailConfig(email,password):
 # except:
 #   mysqlconnection = None
 global mysqlconnection
-
+mysqlconnection = None
 def mysql_connect():
   currentDirectory = os.path.dirname(os.path.abspath(__file__))
   initfile = os.path.join(currentDirectory, 'config.ini')
