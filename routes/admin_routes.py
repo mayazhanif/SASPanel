8b638a0ae04db5c5ad4f0537215d1b848215e019
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-05-07 12:43:39 +0500

    Updated Files

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -11,6 +11,9 @@ from flask import current_app as app
 from datetime import datetime
 from datetime import timedelta
 
+@app.before_request
+def before_request():
+    mysqlconnection.reconnect()
 
 @routes.route('/admin/dashboard')
 def admin_dashboard():
