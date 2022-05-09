37a43aeb149791f1375846dc6732943ac87d6ee3
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-05-09 10:57:15 +0500

    Updated Files

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,2 +1,11 @@
-from flask import Blueprint
+from urllib.parse import urlparse
+from flask import Blueprint,request
+
 routes = Blueprint('routes', __name__)
+@routes.app_context_processor
+def server_host():
+    o = urlparse(request.base_url)
+    hosturl = o.hostname
+    return dict(
+        mainhost = hosturl,
+    )
