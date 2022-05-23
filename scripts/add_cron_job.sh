eefaafe7efa4817a4e1e6040af062d18260fa440
Muhammad Ayaz Hanif <mayazhanif@gmail.com>
2022-05-23 09:01:31 +0500

    Updated Files

--- scripts/add_cron_job.sh
+++ scripts/add_cron_job.sh
@@ -6,6 +6,6 @@ croncommand=$2
 mkdir -m 777 -p /home/$username/crobjobs/logs
 chown -R www-data:www-data /home/$username/crobjobs/logs/*
 chown -R $username:$username /home/$username/crobjobs/logs/*
-echo $croncommand >> mycron
+echo "$croncommand" >> mycron
 crontab -u $username mycron
 rm mycron
